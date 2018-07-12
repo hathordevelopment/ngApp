22a191b4234c434b79946c33fe02b36ba6f46c6a
Alexis Goupillot <hathor.development@gmail.com>
2018-07-12 20:13:42 +0200

    Property binding effectué

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PremierComponent } from './premier/premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremierComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule
